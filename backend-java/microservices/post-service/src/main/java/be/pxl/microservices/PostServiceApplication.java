fa743a50914e34a9403739f72fe1661ec0ea7c0c
RonEerdekensPXL <ron.eerdekens@student.pxl.be>
2024-11-21 14:03:01 +0100

    Add OpenFeign to microservices

--- backend-java/microservices/post-service/src/main/java/be/pxl/microservices/PostServiceApplication.java
+++ backend-java/microservices/post-service/src/main/java/be/pxl/microservices/PostServiceApplication.java
@@ -3,9 +3,11 @@ package be.pxl.microservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class PostServiceApplication
 {
     public static void main( String[] args )
